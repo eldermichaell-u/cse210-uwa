3275f91fdb4784824b727914d3e2cd63fdb9fb40
eldermichaell-u <mic21017@byui.edu>
2023-01-24 08:58:41 +0100

    addded codes

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Resume
+{
+    public string _name = "";
+    
+    //Initiating list of new list
+    public List<Jobs> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        //Use of the custom type "Job" in the loop
+        foreach (Job job in _jobs)
+        {
+
+            //Calls the display method on each job
+
+            job.Display();
+        }
+    }
+
+
+}
